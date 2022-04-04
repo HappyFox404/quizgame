563ba03f6cec7baf513de213af7cfe0a0f14cdda
HappyFox002 <45999287+HappyFox002@users.noreply.github.com>
2022-04-04 18:49:00 +0300

    Add Func

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Search.css';
 
-export default function Search({action}) {
+export default function Search({actionEdit, action}) {
+
+    const [Value,setValue] = useState('');
 
     const Search = () => {
-        if (action)
-            action();
+        if (actionEdit)
+            action(Value);
+    };
+
+    const OnChange = (e) => {
+        e.preventDefault();
+        setValue(e.target.value);
+        if (actionEdit)
+            actionEdit(e.target.value);
     };
 
     return (
         <div className='Search'>
-            <input type='text' placeholder='Поиск' />
-            <div className='Button'>&#9658;</div>
+            <input type='text' placeholder='Поиск' value={Value} onChange={OnChange}/>
+            <div className='Button' onClick={Search}>&#9658;</div>
         </div>
     )
 }
--- src/main/Lobby.jsx
+++ src/main/Lobby.jsx
@@ -1,18 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import List from '../components/List';
 import Search from '../components/Search';
 import './Lobby.css';
 
 export default function Lobby() {
+
+  const [SearchValue,setSearchValue] = useState('');
+
+  const list = [
+    <div>1</div>,
+    <div>2</div>,
+    <div>3</div>,
+    <div>4</div>,
+    <div>5</div>
+  ];
+
+  const SearchEdit = (text) => {
+    console.log(text);
+    setSearchValue(text);
+  };
+
+  const SearchAction = () => {
+    console.log(SearchValue);
+  };
+
   return (
     <div className='Lobby'>
       <div className='Header'>Викторина Online</div>
       <List className='Content' style={{ gridArea: 'Content', margin: '0 20px' }}>
-        <Search/>
-        <div>123</div>
-        <div>321</div>
-        <div>231</div>
-        <div>1222</div>
+        <Search actionEdit={SearchEdit} action={SearchAction}/>
+        {
+          list.map((el) => el)
+        }
       </List>
     </div>
   )
